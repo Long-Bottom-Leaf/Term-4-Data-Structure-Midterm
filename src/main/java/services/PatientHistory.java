92c1bc19f626e795dad69d7ccb1bb531e195c010
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-03-03 19:48:37 -0330

    Building patient history service

--- src/main/java/services/PatientHistory.java
+++ src/main/java/services/PatientHistory.java
@@ -1,5 +1,33 @@
 package services;
 
+import model.PatientRecord;
+import org.w3c.dom.Node;
+
 public class PatientHistory {
 
+    private Node head;
+    private Node tail;
+    private Node current;
+
+    // private inner node
+    private class Node {
+        PatientRecord data;
+        Node next;
+        Node previous;
+
+        Node(PatientRecord data) {
+            this.data = data;
+        }
+    }
+
+    public PatientHistory() {
+        head = null;
+        tail = null;
+        current = null;
+    }
+
+    // add record to end of history
+    public void addRecord(PatientRecord record) {
+
+    }
 }
